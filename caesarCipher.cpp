20e710053e50c40009c8e8e1183555392dc9456f
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2016-02-15 14:34:12 -0600

    Working on input handling

--- caesarCipher.cpp
+++ caesarCipher.cpp
@@ -13,58 +13,125 @@ int main(int argc, char* argv[]){
 
     const int SHIFTAMOUNT = 3;
 
-    ifstream inputFile;
-
     /*
      * Input file handeling
      */
-    bool setDecode;
-    if( (argc >= 3) && ( (string)argv[1] == "-f") ){
-        inputFile.open(argv[2]);
-        if( (argc == 4) && ( (string)argv[3] == "-d") ){
-            setDecode = true;
-        }
-
-    } else {
-        cout << "caesarCipher: usage -- ciphter -f FILENAME" << endl;
+    if ( argc > 5 ){
+        cout << "caesarCipher: Too many input arguments" << endl;
+        exit(EXIT_FAILURE);
+    } else if( argc < 2 ){
+        cout << "caesarCipher: Too few input arguments" << endl;
         exit(EXIT_FAILURE);
     }
 
+    unsigned int i;
+    unsigned int j;
+    int modifier;
+    ifstream inputFile;
+
+    bool setDecode    = false;
+    bool readFromFile = true;
+    for( i = 0; i <= argc; i++ ){
+        // find modifiers in argv
+        if( argv[i][0] == '-' ){
+            // set modifier accordingly
+            for( j = 1; argv[i][j] != '\0'; j++ ){
+                //cout << argv[i][j];
+                modifier = argv[i][j];
+                //cout << "modifier == " << modifier << endl;
+
+                switch (modifier){
+                    case 'o':
+                        //cout << "caesarCipher: Specify output file AFTER "
+                         //   "the input file" << endl;
+                        exit(EXIT_FAILURE);
+                        break;
+                    case 'f':
+                        // specifiy input file
+                        readFromFile = true;
+                        inputFile.open(argv[i+1]);
+                        cout << "Opening file" << endl;
+                        break;
+                    case 'd':
+                        setDecode = true;
+                        //cout << "Decoding" << endl;
+                        break;
+                    default:
+                        //cout << "caesarCipher: Specify input file with -f" 
+                            //<< endl;
+                        exit(EXIT_FAILURE);
+                }
+            }
+            break; // exit outer for loop
+        }
+    }
+
+    std::ofstream outputFile;
+    bool printToFile = false;
+    if( argc == 5){
+        if( (string)argv[3] == "-o" ){
+            printToFile = true;
+            outputFile.open(argv[4]);
+        }
+    } 
+
     if( !inputFile.is_open() ){
-        cout << "caesarCipher: ERROR, input file did not open properly" 
+        cout << "caesarCipher: ERROR, INPUT file did not open properly" 
             << endl;
         exit(EXIT_FAILURE);
+    }
+
+    if( printToFile && !outputFile.is_open() ){
+        cout << "caesarCipher: ERROR, OUTPUT file did not open properly" 
+            << endl;
     }
 
     /*
      * Encryption begins here
      */
     string line;
-    int i;
     //char letter;
-    while( getline(inputFile, line) ){
+    if( readFromFile ){
+        while( getline(inputFile, line) ){
 
-        // encode
-        if( !setDecode ){
-            for( i = 0; i < line.length(); i++ ){
-                if( isLetter(line[i]) ){
-                    line[i] += SHIFTAMOUNT;
+            // encode
+            if( !setDecode ){
+                for( i = 0; i < line.length(); i++ ){
+                    if( isLetter(line[i]) ){
+                        line[i] += SHIFTAMOUNT;
+                    }
+                }
+                if( !printToFile ){
+                    // then print to stdout
+                    cout << line << endl;
+                } else {
+                    outputFile << line;
+                }
+
+            // decode
+            } else {
+                for( i = 0; i < line.length(); i++ ){
+                    if( isLetter(line[i]) ){
+                        line[i] -= SHIFTAMOUNT;
+                    }
+                }
+                if( !printToFile ){
+                    // then print to stdout
+                    cout << line << endl;
+                } else {
+                    outputFile << line;
                 }
             }
-            cout << line << endl;
-
-        // decode
-        } else {
-            for( i = 0; i < line.length(); i++ ){
-                if( isLetter(line[i]) ){
-                    line[i] -= SHIFTAMOUNT;
-                }
-            }
-            cout << line << endl;
         }
+    } else {
+        // read from stdin
+        // TODO: Not yet working
+        line = argv[1];
+        cout << line << endl;
     }
 
     inputFile.close();
+    outputFile.close();
     return 0;
 }
 
